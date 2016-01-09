dbd22baf71e73d3fe948b56a0249a090a92dfa89
Grant Miner <xxgsoftware@gmail.com>
2016-01-09 15:12:41 -0600

    Struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![feature(float_extras)]
+#![feature(test)]
+
+extern crate test;
 extern crate kdtree;
 extern crate csv;
 extern crate rustc_serialize;
@@ -13,8 +16,55 @@ struct Record {
     name: String,
     admin1: String,
     admin2: String,
-    admin3: String
+    admin3: String,
 }
+
+pub struct ReverseGeocoder<'a> {
+    tree: KdTree<'a, &'a Record>,
+    coords: Vec<[f64; 2]>,
+    records: Vec<Record>,
+}
+
+impl<'a> ReverseGeocoder<'a> {
+    // fn new() -> ReverseGeocoder<'a> {
+    //
+    //     return ReverseGeocoder::<'a> {
+    //         tree: KdTree::<'a>::new(2),
+    //         coords: Vec::new(),
+    //         records: Vec::new(),
+    //     }.initialize();
+    // }
+
+    fn new() -> ReverseGeocoder<'a> {
+
+        let r = ReverseGeocoder::<'a> {
+            tree: KdTree::<'a>::new(2),
+            coords: Vec::new(),
+            records: Vec::new(),
+        };
+
+        // r.initialize();
+
+        r
+    }
+
+    fn initialize(&'a mut self) {
+        let mut rdr = csv::Reader::from_file("cities.csv").unwrap();
+
+        for record in rdr.decode() {
+            let r: Record = record.unwrap();
+            self.coords.push([r.lat, r.lon]);
+            self.records.push(r);
+        }
+
+        for i in 0..self.coords.len() {
+            self.tree.add(&self.coords[i], &self.records[i]);
+        }
+
+        println!("Loading complete.");
+    }
+}
+
 
 fn geodetic_in_ecef(geo_coords: (f32, f32)) -> (f32, f32, f32) {
     let a = 6378.137; // major axis in kms
@@ -73,4 +123,21 @@ fn main() {
     let y = search(&kdtree, &[44.962786, -93.344722]).unwrap();
 
     print_record(&y);
+}
+
+
+#[cfg(test)]
+extern crate test;
+
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn it_works() {
+    }
+
+    #[bench]
+    fn bench_lookup(b: &mut Bencher) {
+    }
 }
