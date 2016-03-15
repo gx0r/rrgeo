4f1775403fa406cdc2d9f003116a0c09e654552a
Grant Miner <xxgsoftware@gmail.com>
2016-03-15 18:55:51 -0500

    Unit test and time taken to load CSV file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#![feature(test)]
 extern crate kdtree;
 extern crate csv;
 extern crate rustc_serialize;
+extern crate time;
 
 use kdtree::KdTree;
+use time::PreciseTime;
 
 #[derive(Clone, RustcDecodable)]
 struct Record {
@@ -20,6 +23,7 @@ pub struct Locations {
 
 impl Locations {
     fn from_file() -> Locations {
+        let start = PreciseTime::now();
         let mut records = Vec::new();
 
         let mut rdr = csv::Reader::from_file("cities.csv").unwrap();
@@ -28,6 +32,10 @@ impl Locations {
             let r: Record = record.unwrap();
             records.push(([r.lat, r.lon], r));
         }
+
+        let end = PreciseTime::now();
+
+        println!("{} seconds to load cities.csv", start.to(end));
 
         Locations {
             records: records,
@@ -82,119 +90,30 @@ fn main() {
     print_record(&y);
 }
 
-
-/* old code
-
-
-
-
-
-
-
-
-
 extern crate test;
-extern crate kdtree;
-extern crate csv;
-extern crate rustc_serialize;
-use std::rc::Rc;
-use std::rc::Weak;
-use std::cell::RefCell;
-use kdtree::KdTree;
-// use kdtree::ErrorKind;
 
-#[derive(Clone, RustcDecodable)]
-struct Record {
-    lat: f64,
-    lon: f64,
-    name: String,
-    admin1: String,
-    admin2: String,
-    admin3: String,
-}
+mod tests {
 
-fn print_type_of<T>(_: &T) -> () {
-    let type_name =
-        unsafe {
-            std::intrinsics::type_name::<T>()
-        };
-    println!("{}", type_name);
-}
+    #[test]
+    fn it_works() {
+        let loc = super::Locations::from_file();
 
-pub struct ReverseGeocoder<'a> {
-    tree: KdTree<'a, Box<Record>>,
-    // rows,
-    // coords: Vec<[f64; 2]>,
-    // records: Vec<Record>,
-}
+        let geocoder = super::ReverseGeocoder::new(&loc);
 
-impl<'a> ReverseGeocoder<'a> {
-    fn new() -> ReverseGeocoder<'a> {
-        let mut rg = ReverseGeocoder::<'a> {
-            tree: KdTree::new(2),
-            // coords: Vec::new(),
-            // records: Vec::new(),
-        };
+        let y = geocoder.search(&[44.962786, -93.344722]);
+        assert_eq!(y.is_some(), true);
+        let x = y.unwrap();
 
-        let mut rdr = csv::Reader::from_file("cities.csv").unwrap();
-
-        let rows = rdr.decode().collect::<csv::Result<Vec<Record>>>().unwrap();
-        print_type_of(&rows);
-
-        // let coords = Vec::new();
-        //
-        // for record in rdr.decode() {
-        //     let r: Record = record.unwrap();
-        //     coords.push([r.lat, r.lon]);
-        // }
-        //
-        // let coords: [ [f64; 2]; coords.len()] = [];
-
-        // let coords2 = rg.coords.as_slice();
-
-        // rg.tree.add(rg.coords.get(rg.coords.len() - 1).unwrap(), Box::new(r));
-            // self.records.push(r);
-            // self.tree.add(&self.coords[i], Box::new(r));
-
-        rg
     }
 
-    // fn initialize(&'a mut self) -> &'a ReverseGeocoder {
-    //     let mut rdr = csv::Reader::from_file("cities.csv").unwrap();
+    // #[bench]
+    // fn bench_lookup(b: &mut Bencher) {
     //
-    //     for record in rdr.decode() {
-    //         let r: Record = record.unwrap();
-    //         // self.tree.add(&[r.lat, r.lon], Box::new(r));
-    //         self.coords.push([r.lat, r.lon]);
-    //         self.records.push(r);
-    //         // self.tree.add(&self.coords[i], Box::new(r));
-    //
-    //     }
-    //     //
-    //     // for i in 0..self.coords.len() {
-    //     //     self.tree.add(&self.coords[i], Box::new(self.records[i]));
-    //     // }
-    //     //
-    //     // println!("Loading complete.");
-    //
-    //     self
     // }
-
-    // fn search(&self, loc: &[f64; 2]) -> Option<Record> {
-    //     use kdtree::distance::squared_euclidean;
-    //
-    //     let y = self.tree.nearest(loc, 1, &squared_euclidean).unwrap();
-    //
-    //     if y.len() > 0 {
-    //         return Some((*y[0].1).clone());
-    //     } else {
-    //         return None;
-    //     }
-    // }
-
 }
 
 
+/*
 fn geodetic_in_ecef(geo_coords: (f32, f32)) -> (f32, f32, f32) {
     let a = 6378.137; // major axis in kms
     let e2 = 0.00669437999014;
@@ -212,41 +131,4 @@ fn geodetic_in_ecef(geo_coords: (f32, f32)) -> (f32, f32, f32) {
 
     (x, y, z)
 }
-
-fn print_record(r: &Record) {
-    println!("({}, {}): {} {} {} {}", r.lat, r.lon, r.name, r.admin1, r.admin2, r.admin3);
-}
-
-fn main() {
-    // let coder: Rc<RefCell<ReverseGeocoder>> = Rc::new(RefCell::new(ReverseGeocoder::new()));
-    let coder = ReverseGeocoder::new();
-    // coder.borrow_mut().initialize();
-
-    // let mut coder = ReverseGeocoder::new();
-    // // let i = &mut coder;
-    // coder.initialize();
-    // let y = coder.search(&[44.962786, -93.344722]).unwrap();
-    //
-    // print_record(&y);
-
-}
-
-
-#[cfg(test)]
-extern crate test;
-
-mod tests {
-    use super::*;
-    use test::Bencher;
-
-    #[test]
-    fn it_works() {
-    }
-
-    #[bench]
-    fn bench_lookup(b: &mut Bencher) {
-    }
-}
-
-
 */
