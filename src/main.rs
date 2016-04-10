f13d3a511465a35b54be2b443232dd8f849aa94a
Grant Miner <gx0r@protonmail.com>
2016-04-09 21:01:37 -0500

    Compiler bug

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![feature(custom_derive, plugin, custom_attribute, type_macros)]
+#![plugin(docopt_macros)]
 extern crate kdtree;
 extern crate rustc_serialize;
 extern crate time;
+extern crate docopt;
 
 mod geocoder;
 use geocoder::Locations;
 use geocoder::ReverseGeocoder;
 use geocoder::print_record;
+
+docopt!(Args derivce, "
+rreverse -- offline reverse geocoder
+
+Usage:
+    rreverse lat long
+");
 
 fn main() {
     let loc = Locations::from_file();
