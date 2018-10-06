309648e0bdb04b3084ffc71cef706ab10245799a
Grant Miner <llambda@users.noreply.github.com>
2018-10-06 15:41:40 -0500

    Cleanups

--- actix/src/actix.rs
+++ actix/src/actix.rs
@@ -1,45 +1,32 @@
 #![feature(rustc_private)]
-
+#[macro_use]
+extern crate failure;
 extern crate reversegeocoder;
 #[macro_use]
 extern crate lazy_static;
-extern crate rustc_serialize;
-extern crate time;
-extern crate queryst;
 extern crate actix_web;
+extern crate queryst;
+extern crate rustc_serialize;
 extern crate serde;
+extern crate time;
 #[macro_use]
 extern crate serde_derive;
-use actix_web::{http, server, App, Query, Responder, Json, Result, HttpRequest, HttpResponse, error};
 
-use reversegeocoder::{
-    Locations,
-    ReverseGeocoder,
-    Record,
-};
+use actix_web::{error, http, server, App, HttpResponse, Json, Query, Result};
 
-#[macro_use] extern crate failure;
+use reversegeocoder::{Locations, Record, ReverseGeocoder};
 
 #[derive(Fail, Debug)]
 enum MyError {
-   #[fail(display="internal error")]
-   InternalError,
-   #[fail(display="bad request")]
-   BadClientData,
-   #[fail(display="timeout")]
-   Timeout,
+    #[fail(display = "bad request")]
+    BadClientData,
 }
 
 impl error::ResponseError for MyError {
     fn error_response(&self) -> HttpResponse {
-       match *self {
-          MyError::InternalError => HttpResponse::new(
-              http::StatusCode::INTERNAL_SERVER_ERROR),
-          MyError::BadClientData => HttpResponse::new(
-              http::StatusCode::BAD_REQUEST),
-          MyError::Timeout => HttpResponse::new(
-              http::StatusCode::GATEWAY_TIMEOUT),
-       }
+        match *self {
+            MyError::BadClientData => HttpResponse::new(http::StatusCode::BAD_REQUEST),
+        }
     }
 }
 
@@ -58,17 +45,14 @@ fn index(lat_long: Query<LatLong>) -> Result<Json<Record>, MyError> {
     let res = GEOCODER.search(&[lat_long.lat, lat_long.long]);
 
     match res {
-        Ok(res) => {
-            Ok(Json( (**((*res.get(0).unwrap()).1)).clone() ))
-        },
-        Error => Err(MyError::BadClientData),
+        Ok(res) => Ok(Json((**((*res.get(0).unwrap()).1)).clone())),
+        Err(_e) => Err(MyError::BadClientData),
     }
 }
 
 fn main() {
-    server::new(
-        || App::new()
-        .route("/", http::Method::GET, index))
-        .bind("127.0.0.1:3000").expect("Can not bind to 127.0.0.1:3000")
+    server::new(|| App::new().route("/", http::Method::GET, index))
+        .bind("127.0.0.1:3000")
+        .expect("Can not bind to 127.0.0.1:3000")
         .run();
 }
