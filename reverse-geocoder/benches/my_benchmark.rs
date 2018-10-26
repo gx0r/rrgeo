c3419e4fa6a5e19b7547ab3dd3698c7d3a644ed2
Grant Miner <gx0r@protonmail.com>
2018-10-26 16:29:42 -0500

    Add criterion benchmark

---
+++ reverse-geocoder/benches/my_benchmark.rs
@@ -0,0 +1,23 @@
+#[macro_use] extern crate criterion;
+#[macro_use] extern crate lazy_static;
+extern crate reverse_geocoder;
+
+use reverse_geocoder::{
+    Locations,
+    Record,
+    ReverseGeocoder,
+};
+
+use criterion::Criterion;
+
+lazy_static! {
+    static ref LOCATIONS: Locations = Locations::from_memory();
+    static ref GEOCODER: ReverseGeocoder<'static> = ReverseGeocoder::new(&LOCATIONS);
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("search", |b| b.iter(|| GEOCODER.search(&[45.0, 54.0]) ));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
