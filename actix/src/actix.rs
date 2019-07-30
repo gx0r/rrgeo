a030c3047031c4a93bbc6fc22c7c20d636f00f01
Grant Miner <gx0r@protonmail.com>
2019-07-30 17:57:35 -0500

    Actix 1.0

--- actix/src/actix.rs
+++ actix/src/actix.rs
@@ -10,7 +10,7 @@ extern crate time;
 #[macro_use]
 extern crate serde_derive;
 
-use actix_web::{error, http, server, App, HttpResponse, Json, Query, Result};
+use actix_web::{error, http, middleware, web, App, HttpResponse, HttpServer, Result};
 
 use reverse_geocoder::{
     Locations,
@@ -48,26 +48,26 @@ struct LatLong {
     long: f64,
 }
 
-fn index(lat_long: Query<LatLong>) -> Result<Json<Record>, Error> {
+fn index(lat_long: web::Query<LatLong>) -> Result<web::Json<Record>, Error> {
     let res = GEOCODER.search(&[lat_long.lat, lat_long.long])?;
 
     match res.len() {
         0 => Err(Error::from(MyError::NotFound)),
-        _ => Ok(Json((*((res.get(0).unwrap()).1)).clone())),
+        _ => Ok(web::Json((*((res.get(0).unwrap()).1)).clone())),
     }
 }
 
-fn create_app() -> App {
-    App::new()
-        .route("/", http::Method::GET, index)
-}
-
-
 fn main() {
-    server::new(|| create_app())
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .route("/", web::get().to(index))
+        })
+        .keep_alive(10)
         .bind("127.0.0.1:3000")
         .expect("Can not bind to 127.0.0.1:3000")
-        .run();
+        .run()
+        .expect("Couldn't run");
 }
 
 
@@ -76,20 +76,14 @@ mod tests {
     extern crate bytes;
     use self::bytes::Bytes;
 
-    use actix_web::{http, HttpMessage};
-    use super::create_app;
+    use actix_web::{test, web, App};
+    use super::index;
 
     #[test]
     fn it_serves_results_on_actix() {
-        use actix_web::test::TestServer;
-
-        let mut srv = TestServer::with_factory(create_app);
-
-        let request = srv.client(http::Method::GET, "/?lat=44.962786&long=-93.344722").finish().unwrap();
-        let response = srv.execute(request.send()).unwrap();
-        assert!(response.status().is_success());
-
-        let bytes = srv.execute(response.body()).unwrap();
-        assert_eq!(bytes, Bytes::from_static(b"{\"lat\":44.9483,\"lon\":-93.34801,\"name\":\"Saint Louis Park\",\"admin1\":\"Minnesota\",\"admin2\":\"Hennepin County\",\"admin3\":\"US\"}"));
+        let mut app = test::init_service(App::new().route("/", web::get().to(index)));
+        let req = test::TestRequest::get().uri("/?lat=44.962786&long=-93.344722").to_request();
+        let resp = test::read_response(&mut app, req);
+        assert_eq!(resp, Bytes::from_static(b"{\"lat\":44.9483,\"lon\":-93.34801,\"name\":\"Saint Louis Park\",\"admin1\":\"Minnesota\",\"admin2\":\"Hennepin County\",\"admin3\":\"US\"}"));
     }
 }
