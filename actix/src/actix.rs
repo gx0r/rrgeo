ebbb35094b84a5d117018a37837495f6a99d6caf
Grant Miner <gx0r@protonmail.com>
2019-12-26 22:33:48 -0600

    actix-web 2.0.0

--- actix/src/actix.rs
+++ actix/src/actix.rs
@@ -3,14 +3,14 @@ extern crate failure;
 extern crate reverse_geocoder;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
 extern crate actix_web;
 extern crate queryst;
 extern crate serde;
 extern crate time;
 #[macro_use]
 extern crate serde_derive;
-
-use actix_web::{error, http, middleware, web, App, HttpResponse, HttpServer, Result};
+use actix_web::{error, http, middleware, web, App, HttpServer, HttpResponse, Result};
 
 use reverse_geocoder::{
     Locations,
@@ -48,7 +48,7 @@ struct LatLong {
     long: f64,
 }
 
-fn index(lat_long: web::Query<LatLong>) -> Result<web::Json<Record>, Error> {
+async fn index(lat_long: web::Query<LatLong>) -> Result<web::Json<Record>, Error> {
     let res = GEOCODER.search(&[lat_long.lat, lat_long.long])?;
 
     match res.len() {
@@ -57,33 +57,49 @@ fn index(lat_long: web::Query<LatLong>) -> Result<web::Json<Record>, Error> {
     }
 }
 
-fn main() {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .wrap(middleware::Compress::default())
+            // .wrap(middleware::Logger::default())
             .route("/", web::get().to(index))
-        })
-        .keep_alive(10)
-        .bind("127.0.0.1:3000")
-        .expect("Can not bind to 127.0.0.1:3000")
-        .run()
-        .expect("Couldn't run");
+    })
+    .keep_alive(10)
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
-
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     extern crate bytes;
     use self::bytes::Bytes;
 
-    use actix_web::{test, web, App};
+    use actix_web::dev::Service;
+    use actix_web::{http, test, web, App};
     use super::index;
 
-    #[test]
-    fn it_serves_results_on_actix() {
-        let mut app = test::init_service(App::new().route("/", web::get().to(index)));
+    #[actix_rt::test]
+    async fn it_serves_results_on_actix() -> Result<(), Error> {
+        let mut app = test::init_service(
+            App::new().route("/", web::get().to(index))
+        )
+        .await;
+
         let req = test::TestRequest::get().uri("/?lat=44.962786&long=-93.344722").to_request();
-        let resp = test::read_response(&mut app, req);
-        assert_eq!(resp, Bytes::from_static(b"{\"lat\":44.9483,\"lon\":-93.34801,\"name\":\"Saint Louis Park\",\"admin1\":\"Minnesota\",\"admin2\":\"Hennepin County\",\"admin3\":\"US\"}"));
+
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(*response_body, Bytes::from_static(b"{\"lat\":44.9483,\"lon\":-93.34801,\"name\":\"Saint Louis Park\",\"admin1\":\"Minnesota\",\"admin2\":\"Hennepin County\",\"admin3\":\"US\"}"));
+
+        Ok(())
     }
 }
