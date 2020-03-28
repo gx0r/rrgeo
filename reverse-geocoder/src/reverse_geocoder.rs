13292cff6fadb83b15544758fa05b6d9fec40e89
Grant Miner <gx0r@protonmail.com>
2020-03-28 18:31:51 -0500

    Add documentation

--- reverse-geocoder/src/reverse_geocoder.rs
+++ reverse-geocoder/src/reverse_geocoder.rs
@@ -1,3 +1,29 @@
+#![deny(missing_docs)]
+//!A library for fast, offline reverse geocoding. The location data are from [GeoNames](http://www.geonames.org/).
+//!
+//! # Usage
+//!First, add this to your Cargo.toml
+//!
+//! ```toml
+//! [dependencies]
+//! reverse_geocoder = "^1.0.0"
+//! ```
+//!
+//! Next:
+//!
+//! ```
+//! use reverse_geocoder::{Locations, ReverseGeocoder};
+//!
+//! fn main() {
+//!     let loc = Locations::from_memory();
+//!     let geocoder = ReverseGeocoder::new(&loc);
+//!     let y = geocoder.search(&[45.0, 54.0]).expect("Search error.");
+//!     let pair = y.get(0).expect("No results.");
+//!     println!("Distance {}", pair.0);
+//!     println!("Record {}", pair.1);
+//! }
+//!```
+
 #[macro_use]
 extern crate failure;
 #[macro_use]
@@ -9,21 +35,30 @@ use failure::Error;
 use std::fmt;
 use std::path::PathBuf;
 
+/// A parsed location.
 #[derive(Debug, Clone, RustcDecodable, RustcEncodable, Serialize, Deserialize)]
 pub struct Record {
+    /// Latitude
     pub lat: f64,
+    /// Longitude
     pub lon: f64,
+    /// Location name
     pub name: String,
+    /// Administrative district 1
     pub admin1: String,
+    /// Administrative district 2
     pub admin2: String,
+    /// Administrative district 3
     pub admin3: String,
 }
 
+/// A set of location records.
 pub struct Locations {
     records: Vec<([f64; 2], Record)>,
 }
 
 impl Locations {
+    /// Use the built-in cities.csv.
     pub fn from_memory() -> Locations {
         let mut records = Vec::new();
         let my_str = include_str!("../cities.csv");
@@ -36,6 +71,7 @@ impl Locations {
         Locations { records: records }
     }
 
+    /// Supply your own path to a CSV file.
     pub fn from_path(path: Option<PathBuf>) -> Result<Locations, Error> {
         // let start = Instant::now();
         let mut records = Vec::new();
@@ -60,11 +96,13 @@ impl Locations {
     }
 }
 
+/// A reverse geocoder.
 pub struct ReverseGeocoder<'a> {
     tree: KdTree<f64, &'a Record, &'a [f64; 2]>,
 }
 
 impl<'a> ReverseGeocoder<'a> {
+    /// Create a new reverse geocoder from a set of locations.
     pub fn new(loc: &'a Locations) -> ReverseGeocoder<'a> {
         let mut reverse_geocoder = ReverseGeocoder::<'a> {
             tree: KdTree::with_capacity(2, loc.records.len()),
@@ -82,6 +120,7 @@ impl<'a> ReverseGeocoder<'a> {
         // println!("{} ms to build the KdTree", (end - start).whole_milliseconds());
     }
 
+    /// Search for the closest record to a given lat/long. Returns Result<Vec<(distance, record)>>.
     pub fn search(&self, loc: &[f64; 2]) -> Result<Vec<(f64, &&Record)>, ErrorKind> {
         self.tree.nearest(loc, 1, &squared_euclidean)
     }
