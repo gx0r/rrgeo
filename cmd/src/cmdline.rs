dd464bc7664db2a4d076583a49c13129e6a07fe6
Grant Miner <gx0r@protonmail.com>
2020-04-11 20:31:49 -0500

    main() return Result

--- cmd/src/cmdline.rs
+++ cmd/src/cmdline.rs
@@ -1,8 +1,9 @@
-use reverse_geocoder::{Locations, ReverseGeocoder};
+use reverse_geocoder::{Locations, ReverseGeocoder, SearchResult};
 use std::{env, process::exit};
+use std::error::Error;
 use time::Instant;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<_> = env::args().collect();
 
     if args.len() < 3 {
@@ -10,8 +11,8 @@ fn main() {
         exit(1);
     }
 
-    let lat = args[1].parse::<f64>().expect("Couldn't parse latitude");
-    let long = args[2].parse::<f64>().expect("Couldn't parse longitude");
+    let lat = args[1].parse::<f64>()?;
+    let long = args[2].parse::<f64>()?;
 
     let loc = Locations::from_memory();
     let geocoder = ReverseGeocoder::new(&loc);
@@ -22,4 +23,6 @@ fn main() {
 
     println!("Location: {}", search_result.record);
     println!("Distance: {}", search_result.distance);
+
+    Ok(())
 }
